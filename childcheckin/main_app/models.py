a50290fa75f97e672ffc1528ae871ad48d1a252b
kelvinbest1 <kelvinlanier@yahoo.com>
2023-04-18 18:02:42 -0400

    updating view.py

--- childcheckin/main_app/models.py
+++ childcheckin/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -9,6 +10,9 @@ class Child(models.Model):
 
 def __str__(self):
     return self.name
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'child_id': self.id})
 
 
 class Roster(models.Model):
