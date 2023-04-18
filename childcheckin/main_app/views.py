3917950ccccf16777292903428575c23967bf5e6
kelvinbest1 <kelvinlanier@yahoo.com>
2023-04-18 16:33:00 -0400

    updating views.py

--- childcheckin/main_app/views.py
+++ childcheckin/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Child
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Child, Roster
 
 # Create your views here.
 def home(request):
@@ -15,5 +17,15 @@ def children_index(request):
 def children_detail(request, child_id):
   child = Child.objects.get(id=child_id)
   return render(request, 'children/detail.html', {'child': child,})
+
+class RosterCreate(LoginRequiredMixin, CreateView):
+  model = Roster
+  fields = ['date', 'age_group', 'checkin_status', 'caregiver', 'dropped_off_by', 'enrolled_status']
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+   
+   
 
   
