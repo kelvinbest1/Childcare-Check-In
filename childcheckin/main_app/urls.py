0c48a4de09e64210aa82c19608748a3452e51585
kelvinbest1 <kelvinlanier@yahoo.com>
2023-04-19 19:51:30 -0400

    updating url.py

--- childcheckin/main_app/urls.py
+++ childcheckin/main_app/urls.py
@@ -13,6 +13,10 @@ path('children/<int:child_id>/add_roster/', views.add_roster, name='add_roster')
 path('activities/', views.ActivityList.as_view(), name='activites_index'),
 path('children/<int:child_id>/assoc_activity/<int:activity_id>/', views.assoc_activity, name='assoc_activity'),
 path('children/<int:child_id>/unassoc_activities/<int:activity_id>/', views.unassoc_activity, name='unassoc_activity'),
+path('activities/<int:pk>/', views.ActivityDetail.as_view(), name='activities_detail'),
+path('activities/create/', views.ActivityCreate.as_view(), name='activities_create'),
+path('activities/<int:pk>/update/', views.ActivityUpdate.as_view(), name='activities_update'),
+path('activities/<int:pk>/delete/', views.ActivityDelete.as_view(), name='activities_delete'),
 path('accounts/signup/', views.signup, name='signup'),
 
 
